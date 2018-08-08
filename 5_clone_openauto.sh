b8cf783e649d453f6d6f4bdfc1fc9d164c45b64d
petterhj <post@petterhj.net>
2018-08-08 19:43:02 +0000

    created openauto clone script

---
+++ 5_clone_openauto.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "================================================================"
+echo "Step 2: Clone openauto repository"
+echo "================================================================"
+
+WORK_DIR=$HOME/openauto
+OPENAUTO_SOURCE_DIR=$WORK_DIR/openauto
+OPENAUTO_REPO="https://github.com/opencardev/openauto.git"
+OPENAUTO_BRANCH="crankshaft-ng"
+
+echo "> Working directory: $WORK_DIR"
+echo "> OpenAuto source directory: $OPENAUTO_SOURCE_DIR"
+
+# Remove any existing source files
+if [ -d "$OPENAUTO_SOURCE_DIR" ]; then
+	echo "> Removing existing source files from $OPENAUTO_SOURCE_DIR"
+	sudo rm -rf $OPENAUTO_SOURCE_DIR
+fi
+
+# Change directory
+echo "> Changing directory to $WORK_DIR"
+
+cd $WORK_DIR
+
+# Clone git repository
+echo "> Cloning $OPENAUTO_REPO [$OPENAUTO_BRANCH]"
+
+git clone -b $OPENAUTO_BRANCH $OPENAUTO_REPO
