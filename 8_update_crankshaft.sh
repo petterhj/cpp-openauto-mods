abc7677eaa193e906e2f1c46a71adfc81f4c220d
petterhj <post@petterhj.net>
2018-08-15 19:53:53 +0000

    collect and upload

--- 8_update_crankshaft.sh
+++ 8_update_crankshaft.sh
@@ -10,6 +10,8 @@ AASDK_BUILD_DIR=$WORK_DIR/aasdk_build
 OPENAUTO_SOURCE_DIR=$WORK_DIR/openauto
 OPENAUTO_BUILD_DIR=$WORK_DIR/openauto_build
 COLLECT_DIR=$WORK_DIR/collected
+TARGET_SYSTEM=pi@192.167.1.17
+TARGET_PATH=/home/pi
 
 echo "> Working directory: $WORK_DIR"
 echo "> Collect directory: $COLLECT_DIR"
@@ -23,9 +25,13 @@ fi
 # Create collect directory
 echo "> Creating collect directory"
 mkdir $COLLECT_DIR
-# mkdir $COLLECT_DIR/lib
-# mkdir $COLLECT_DIR/bin
 
 # Collect built binaries/libraries
 echo "> Collecting built binaries and libraries"
 
+ cp -v $AASDK_SOURCE_DIR/lib/libaasdk.so $COLLECT_DIR
+ cp -v $AASDK_SOURCE_DIR/lib/libaasdk_proto.so $COLLECT_DIR
+ cp -v $OPENAUTO_SOURCE_DIR/bin/autoapp $COLLECT_DIR
+ cp -v $OPENAUTO_SOURCE_DIR/bin/btservice $COLLECT_DIR
+
+echo "> Done collecting built files"
---
+++ 9_file_upload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "================================================================"
+echo "Step 9: Upload files"
+echo "================================================================"
+
+WORK_DIR=$HOME/openauto
+COLLECT_DIR=$WORK_DIR/collected
+TARGET_SYSTEM=pi@192.167.1.17
+TARGET_PATH=/home/pi
+
+echo "> Working directory: $WORK_DIR"
+echo "> Collect directory: $COLLECT_DIR"
+echo "> Target system: $TARGET_SYSTEM"
+echo "> Target path: $TARGET_PATH"
+
+# Upload to crankshaft system
+echo "> Uploading to crankshaft system"
+scp -r $COLLECT_DIR $TARGET_SYSTEM:$TARGET_PATH
